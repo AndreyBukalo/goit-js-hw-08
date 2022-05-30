dc2b13de6b1abdcc9bc246cc7635d6ef08842005
AndreyBukalo <bukaloandrey@gmail.com>
2022-05-30 16:38:24 +0300

    Upd
    Lightbox installed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery">
+    <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        
+        alt="${description}"
+      />
+    </a>
+  </div>
+ `
+  )
+  .join('');
+
+gallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
