0570e651e5f749f507f104f427e1fb4d832f2552
AndreyBukalo <bukaloandrey@gmail.com>
2022-06-02 09:11:09 +0300

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,21 @@
-import Player from '@vimeo/player';
-
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function (data) {
-      duration: 61.857;
-      percent: 0.049;
-      seconds: 3.034;
-  
-  console.log('played the video!');
-});
+const KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+    const dataJson = JSON.stringify(data);
+    localStorage.setItem(KEY,dataJson);
+    
+};
+
+player.on('timeupdate',onPlay);
+   
+
+
+
+
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
-});
-
-
-player.setCurrentTime(30.456).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
 });
