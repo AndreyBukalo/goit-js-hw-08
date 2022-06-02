ccd7dccfa452a0c5340231cc779d4fb54d662056
AndreyBukalo <95246177+AndreyBukalo@users.noreply.github.com>
2022-06-02 09:21:14 +0000

    Deploying to gh-pages from @ AndreyBukalo/goit-js-hw-08@3c684941ee4c7af2dd84d7555dba5ceaff105c22 🚀

--- 02-video.bcdbfdab.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeupdate",(function(e){var t=JSON.stringify(e);localStorage.setItem("videoplayer-current-time",t)})),t.getVideoTitle().then((function(e){console.log("title:",e)}))}();
-//# sourceMappingURL=02-video.bcdbfdab.js.map
